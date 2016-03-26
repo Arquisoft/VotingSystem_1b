ad5bd6ca5344956e4fdc32c6334bf71d2c4020df
David Sánchez <davidslufe02@gmail.com>
2016-03-26 15:08:45 +0100

    Comit david desde casa
    Ver como Redirigir

--- src/main/java/es/uniovi/asw/Main.java
+++ src/main/java/es/uniovi/asw/Main.java
@@ -1,16 +1,20 @@
 package es.uniovi.asw;
 
+import java.util.ArrayList;
+
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.ServletContextAware;
 
 import com.sun.faces.config.ConfigureListener;
@@ -29,9 +33,14 @@ public class Main extends SpringBootServletInitializer implements ServletContext
         ServletRegistrationBean registration = new ServletRegistrationBean(
                 new FacesServlet(), "*.xhtml");
         registration.setLoadOnStartup(1);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("/"); //ya no da el error. Descubrir como redirigir a index?
+        registration.setUrlMappings(list);
         return registration;
     }
 
+    
+    
     @Bean
     public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
         return new ServletListenerRegistrationBean<ConfigureListener>(
@@ -43,5 +52,8 @@ public class Main extends SpringBootServletInitializer implements ServletContext
         servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
         servletContext.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION", "true");
         servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
+        
     }
+    
+ 
 }
