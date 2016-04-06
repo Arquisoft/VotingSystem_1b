85b35e77d8e4845f5aad72597b956abd08c337f7
Xurxo García <xurxo9910@gmail.com>
2016-04-07 01:33:51 +0200

    Vote

---
+++ src/main/java/es/uniovi/asw/dbupdate/model/Vote.java
@@ -0,0 +1,43 @@
+package es.uniovi.asw.dbupdate.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Vote {
+
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		private Long id;
+		private Date fecha;
+		private String codigoColegio;
+		private String opcion;
+		
+		protected Vote(){
+			
+		}
+		
+		public Vote(Date fecha, String codigoColegio, String opcion){
+			this.fecha=fecha;
+			this.codigoColegio=codigoColegio;
+			this.opcion=opcion;
+		}
+
+		public Date getFecha() {
+			return fecha;
+		}
+		
+		public String getCodigoColegio() {
+			return codigoColegio;
+		}
+
+		public String getOpcion() {
+			return opcion;
+		}
+		
+	}
