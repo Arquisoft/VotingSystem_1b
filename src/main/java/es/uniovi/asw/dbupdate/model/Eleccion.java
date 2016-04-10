db1aad55c49cfacea328f06795a0eb82985c9826
David Sánchez <davidslufe02@gmail.com>
2016-04-10 23:54:58 +0200

    HELP: Modelo basico
    hay cosas que aun fallan pero la base es esta

---
+++ src/main/java/es/uniovi/asw/dbupdate/model/Eleccion.java
@@ -0,0 +1,19 @@
+package es.uniovi.asw.dbupdate.model;
+
+public class Eleccion {
+
+		private String nombreEleccion;
+		private String descripcion;
+		private Configuration config;
+		public Eleccion(String nombreEleccion, String descripcion,
+				Configuration config) {
+			super();
+			this.nombreEleccion = nombreEleccion;
+			this.descripcion = descripcion;
+			this.config = config;
+		}
+		
+		
+		
+
+}
