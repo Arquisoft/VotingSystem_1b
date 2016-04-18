b49965e65978d9c062c8e3b2b0e75fa58a982f0a
Santiago Martin <santiagomartinagra@gmail.com>
2016-04-18 12:27:10 +0200

    Ya se puede votar

--- src/main/java/es/uniovi/asw/voter/application/bussiness/impl/SimpleApplicationService.java
+++ src/main/java/es/uniovi/asw/voter/application/bussiness/impl/SimpleApplicationService.java
@@ -4,16 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import es.uniovi.asw.dbupdate.model.ConfigurationElection;
-import es.uniovi.asw.dbupdate.model.ElectoralCollege;
 import es.uniovi.asw.dbupdate.model.TelematicVoter;
 import es.uniovi.asw.dbupdate.model.User;
-import es.uniovi.asw.dbupdate.model.Vote;
 import es.uniovi.asw.dbupdate.repositories.TelematicVoterDAO;
 import es.uniovi.asw.dbupdate.repositories.UserDAO;
 import es.uniovi.asw.dbupdate.repositories.VoteDAO;
 import es.uniovi.asw.voter.application.bussiness.ApplicationService;
-import es.uniovi.asw.voter.application.exception.AlredyVotedException;
-import es.uniovi.asw.voter.application.exception.BusinessException;
 import es.uniovi.asw.voter.application.exception.InvalidUserException;
 
 @Component
@@ -41,34 +37,5 @@ public class SimpleApplicationService implements ApplicationService {
 		td.save(telematic);
 
 	}
-
-	@Override
-	public void voteOnElection(ConfigurationElection c, Vote v, User u,
-			ElectoralCollege colegioElectoral) throws InvalidUserException, BusinessException, AlredyVotedException {
-	
-		User user = ud.findByMailAndContrasena(u.getMail(), u.getContrasena());
-
-		if(user == null){
-			throw new InvalidUserException();
-		}
-		
-		TelematicVoter miVotoTelematico=td.findByUserAndConfigurationElection(u, c);
-		
-		if(miVotoTelematico == null){
-			throw new BusinessException("No habias pedido voto telematico");
-		}
-		
-		if(miVotoTelematico.isVoted()){
-			throw new AlredyVotedException("No habias pedido voto telematico");
-		}
-		
-		
-		miVotoTelematico.setVoted(true);
-		td.save(miVotoTelematico);
-		
-		votoDao.save(v);
-		
-	}
-
 	
 }
